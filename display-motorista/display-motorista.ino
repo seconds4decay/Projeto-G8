21bf5e1a18bd8c52a38a296f49514f32c68af097
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2023-11-21 14:39:12 -0300

    Update display-motorista.ino

--- display-motorista/display-motorista.ino
+++ display-motorista/display-motorista.ino
@@ -1,20 +1,26 @@
 #include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(???, 16, 2);
-SoftwareSerial mySerial(0,1);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+SoftwareSerial mySerial(9,10);
 
-int data;
+String data;
 
 void setup() {
-  mySerial.begin(9600)
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  lcd.init();
+  lcd.clear();
 }
 
 void loop() {
+  lcd.noBacklight();
+  lcd.print("teste");
   if (mySerial.available()) {
-    int data = mySerial.read();
-
+    Serial.println("recebeu");
+    Serial.println(data);
     lcd.backlight();
-    lcd.setCursor(3, 0)
+    lcd.setCursor(3, 0);
+    lcd.print("teste");
   }
 }
