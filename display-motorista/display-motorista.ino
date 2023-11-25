e7e33763a1e010854afa0fe57af3ff68854d7369
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2023-11-25 19:56:46 -0300

    Update display-motorista.ino

--- display-motorista/display-motorista.ino
+++ display-motorista/display-motorista.ino
@@ -1,20 +1,20 @@
 #include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-SoftwareSerial mySerial(9, 10);
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // Configuração da tela LCD 16x2
+SoftwareSerial mySerial(9, 10); // Configuração da comunicação entre os dois arduinos
 
 String data;
 
 void setup() {
-  mySerial.begin(9600);
-  Serial.begin(9600);
-  lcd.init();
-  lcd.clear();
+  mySerial.begin(9600); // Inicialização da comunicação entre os dois arduinos
+  Serial.begin(9600);  // Inicialização do Serial
+  lcd.init(); // Inicialização da tela LCD
+  lcd.clear(); 
 }
 
 void loop() {
-    if (mySerial.available()) {
+    if (mySerial.available()) { // Quando receber um sinal pela comunicação, continua.
       lcd.backlight();
       Serial.println("recebeu");
       Serial.println(data);
@@ -24,7 +24,7 @@ void loop() {
       delay(7000);
       lcd.clear();
       lcd.noBacklight();
-      mySerial.end();
-    mySerial.begin(9600);
+      mySerial.end(); // Finalizar comunicação 
+    mySerial.begin(9600); // Renicializar comunicação e aguardar um proximo sinal
   }
 }
